37704301d832a6cb07ede84d517a335f6576c8ad
harsh goyal <18harshgoyal@gmail.com>
2025-12-05 21:39:22 +0530

    add code

---
+++ dynamic programming/firsttype.java/stairCase.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class stairCase{
+  //only 1 and 2 steps are allowed
+  //recursion method
+  public static int recurStair(int n){
+    if(n<0){
+      return 0;
+    }
+    if(n==0){
+      return 1;
+    }
+   
+    return recurStair(n-1)+recurStair(n-2);
+  }
+  //memoization method
+  public static int memoizationStair(int n,int ways[]){
+    if(n<0){
+      return 0;
+    }
+    if(n==0){
+      return 1;
+    }
+    if(ways[n]!=-1){
+      return ways[n];
+    }
+    ways[n]=memoizationStair(n-1,ways)+memoizationStair(n-2,ways);
+    return ways[n];
+  }
+  //tabular method
+  public static int tabulationfib(int n){
+   int dp [] = new int [n+1];
+   dp[0]=1;
+   
+   for(int i=1 ;i<=n ;i++){
+    if(i==1){
+      dp[i]=dp[i-1];
+    }
+    else{
+    dp[i]= dp[i-1]+dp[i-2];
+    }
+   }
+   return dp[n];//ans
+  }
+  public static void main(String args[]){
+     System.out.print ("enter the number of floor:  ");
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int ways[] = new int [n+1];
+     Arrays.fill(ways,-1);
+     System.out.println("Staircase count ways using recursion:");
+     System.out.println(recurStair(n));
+     System.out.println("Staircase count ways using memoization:");
+     System.out.println(memoizationStair(n, ways));
+     System.out.println("Fibonacci using tabulation:");
+     System .out.println(tabulationfib(n));
+  }
+
+}
